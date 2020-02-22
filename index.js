e829fac701f395aac1388e595f1c6630588a88a2
Jackie Sze <jackiesze0@gmail.com>
2020-02-22 00:05:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
 }
+
+	function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object)
+  delete newObj[key]
+  return newObj
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
